8dbbee8b432f96fc9a8e1ab473c636f25bf799e1
janghayoung <wkdgkdud1005@gmail.com>
2026-05-10 10:37:28 +0900

    Initial Commit

---
+++ MacStandard/MacStandardApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacStandardApp.swift
+//  MacStandard
+//
+//  Created by 하영 on 5/10/26.
+//
+
+import SwiftUI
+
+@main
+struct MacStandardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
